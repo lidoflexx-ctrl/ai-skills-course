3e3d68a2a724f77d0b3ef3a1bbd127fb3e52bbb1
lidoflexx-ctrl <lidoflexx@gmail.com>
2025-11-18 11:28:49 +0200

    Create success.js

---
+++ pages/success.js
@@ -0,0 +1,8 @@
+export default function Success() {
+  return (
+    <div style={{ padding: 40, fontFamily: "Inter, system-ui" }}>
+      <h1>Payment successful</h1>
+      <p>Check your email for access!</p>
+    </div>
+  );
+}
